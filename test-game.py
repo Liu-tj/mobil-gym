b8005554d99078271caea63fbae20a651c3a1fd9
noah <noah.nobel@kakaobrain.com>
2018-09-15 22:15:50 +0900

    add display

--- test-game.py
+++ test-game.py
@@ -1,6 +1,7 @@
 
 
 import pygame
+import numpy as np
 import random, sys
 from time import sleep
 from pygame.locals import *
@@ -40,6 +41,15 @@ CAR_IMG = pygame.image.load('./image/car-small.png')
 ## Game Parameter
 GRID_SIZE = 5
 
+## Array origin call prob
+LOW_PROB = 0.1
+HIGH_PROB = 0.4
+origin_prob = [LOW_PROB, LOW_PROB, LOW_PROB, LOW_PROB, LOW_PROB,
+               LOW_PROB, HIGH_PROB, HIGH_PROB, HIGH_PROB, LOW_PROB,
+               LOW_PROB, HIGH_PROB, HIGH_PROB, HIGH_PROB, LOW_PROB,
+               LOW_PROB, HIGH_PROB, HIGH_PROB, HIGH_PROB, LOW_PROB,
+               LOW_PROB, LOW_PROB, LOW_PROB, LOW_PROB, LOW_PROB]
+
 def main():
     global FPSCLOCK, DISPLAYSURF, BASICFONT, RESET_SURF, RESET_RECT, NEW_SURF, NEW_RECT, SOLVE_SURF, SOLVE_RECT
 
@@ -70,19 +80,39 @@ def main():
 
     tmp = 0
 
+    total_frame = 0
+    frame = 0
+
+    origin_call_list = makeOriginProb(500)
+
     while True:  # main game loop
 
         DISPLAYSURF.fill(WHITE)
 
         displayGrid(DISPLAYSURF, grid=5)
 
+        if total_frame > 499:
+            total_frame = 0
+        else:
+            total_frame = total_frame + 1
+
+        if total_frame % 20 == 0:
+            if frame > 499 :
+                frame = 0
+            else:
+                frame = frame + 1
+
+        displayCallRect(DISPLAYSURF, origin_call_list[frame])
+
+        #displayArrow(DISPLAYSURF, 200, 200)
+
         if tmp > 99:
             tmp =0
         else :
             tmp = tmp +1
 
-        car_x = init_x + tmp
-        car_y = init_y + tmp
+        car_x = init_x-30# + tmp
+        car_y = init_y-30# + tmp
 
         DISPLAYSURF.blit(CAR_IMG, (car_x, car_y))
 
@@ -113,6 +143,42 @@ def displayText(surf, text, center_x, center_y):
     textRectObj = textSurfaceObj.get_rect()
     textRectObj.center = (center_x, center_y)
     surf.blit(textSurfaceObj, textRectObj)
+
+def displayArrow(surf, end_x, end_y):
+
+    pygame.draw.line(surf, RED, (end_x-12, end_y), (end_x, end_y), 3)
+    pygame.draw.line(surf, RED, (end_x-4, end_y-4), (end_x, end_y), 3)
+    pygame.draw.line(surf, RED, (end_x-4, end_y+4), (end_x, end_y), 3)
+
+
+def displayCallRect(surf, tmp_list, grid=5):
+
+    start_x = WINDOWWIDTH/2 - (grid/2)*RECTSIZE
+    start_y = WINDOWHEIGHT/2 - (grid/2)*RECTSIZE
+
+    for i in range(grid):
+        for j in range(grid):
+            rect_x = start_x+i*(RECTSIZE-RECT_LINE_WIDTH/2)
+            rect_y = start_y+j*(RECTSIZE-RECT_LINE_WIDTH/2)
+            if tmp_list[i+5*j] == 1:
+                pygame.draw.rect(surf, RED, (rect_x , rect_y , RECTSIZE, RECTSIZE), RECT_LINE_WIDTH)
+
+
+
+
+def makeOriginProb(max_seq, RANDOM_SEED=42):
+
+    rtn_array = []
+
+    for i in range(max_seq):
+        tmp_call = []
+        for tmp_prob in origin_prob:
+            tmp = np.random.binomial(1, tmp_prob, size=1)
+            tmp_call.append(tmp)
+
+        rtn_array.append(tmp_call)
+
+    return rtn_array
 
 def makeText(text, color, bgcolor, top, left):
     # create the Surface and Rect objects for some text.
