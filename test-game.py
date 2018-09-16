c6b3285afb582b2b258d432f4fd7240467c78daa
Sue Hun Jung <giallo.hos@gmail.com>
2018-09-16 20:21:25 +0900

    update score add

--- test-game.py
+++ test-game.py
@@ -39,13 +39,22 @@ MESSAGECOLOR = WHITE
 GRID_SIZE = 5
 
 ## Array origin call prob
-LOW_PROB = 0.1
-HIGH_PROB = 0.4
+LOW_PROB = 0.08
+HIGH_PROB = 0.25
 origin_prob = [LOW_PROB, LOW_PROB, LOW_PROB, LOW_PROB, LOW_PROB,
                LOW_PROB, HIGH_PROB, HIGH_PROB, HIGH_PROB, LOW_PROB,
                LOW_PROB, HIGH_PROB, HIGH_PROB, HIGH_PROB, LOW_PROB,
                LOW_PROB, HIGH_PROB, HIGH_PROB, HIGH_PROB, LOW_PROB,
                LOW_PROB, LOW_PROB, LOW_PROB, LOW_PROB, LOW_PROB]
+
+D_LOW_PB = 0.02
+D_HIGH_PB = 0.07
+
+destin_prob = [D_LOW_PB, D_LOW_PB, D_LOW_PB, D_LOW_PB, D_LOW_PB,
+               D_LOW_PB, D_LOW_PB, D_HIGH_PB, D_LOW_PB, D_LOW_PB,
+               D_LOW_PB, D_HIGH_PB, D_HIGH_PB, D_HIGH_PB, D_LOW_PB,
+               D_LOW_PB, D_HIGH_PB, D_HIGH_PB, D_HIGH_PB, D_LOW_PB,
+               D_LOW_PB, D_HIGH_PB, D_HIGH_PB, D_HIGH_PB, D_LOW_PB]
 
 def main():
     global FPSCLOCK, DISPLAYSURF, BASICFONT, RESET_SURF, RESET_RECT, NEW_SURF, NEW_RECT, SOLVE_SURF, SOLVE_RECT
@@ -64,7 +73,7 @@ def main():
     SOLVE_SURF, SOLVE_RECT = makeText('Solve', TEXTCOLOR, TILECOLOR, WINDOWWIDTH - 120, WINDOWHEIGHT - 30)
 
     ## Image Load
-    CAR_IMG = pygame.image.load('./image/car-small.png')
+    #CAR_IMG = pygame.image.load('./image/car-small.png')
     CAR_A_IMG = pygame.image.load('./image/car-small_a.png')
     CAR_B_IMG = pygame.image.load('./image/car-small_b.png')
     CAR_C_IMG = pygame.image.load('./image/car-small_c.png')
@@ -112,8 +121,11 @@ def main():
                 frame = 0
             else:
                 frame = frame + 1
+                c_status = updateStatus(c_status, origin_call_list[frame])
 
         displayCallRect(DISPLAYSURF, origin_call_list[frame])
+
+
 
         #displayArrow(DISPLAYSURF, 200, 200)
 
@@ -161,10 +173,10 @@ def displayScore(surf, c_status):
         tmp_dist = str(c_status[i]['dist'])
         tmp_income = str(c_status[i]['income'])
 
-        surf.blit(font_stats.render(tmp_name, False, BLACK), (10, 25+i*20))
-        surf.blit(font_stats.render(tmp_trip, False, GREEN), (80, 25+i*20))
-        surf.blit(font_stats.render(tmp_dist, False, GREEN), (130, 25+i*20))
-        surf.blit(font_stats.render(tmp_income, False, GREEN), (180, 25+i*20))
+        surf.blit(font_stats.render(tmp_name, False, BLACK), (10, 28+i*20))
+        surf.blit(font_stats.render(tmp_trip, False, BLUE), (80, 28+i*20))
+        surf.blit(font_stats.render(tmp_dist, False, BLUE), (130, 28+i*20))
+        surf.blit(font_stats.render(tmp_income, False, BLUE), (180, 28+i*20))
 
 
 def initStatus(num_car):
@@ -176,18 +188,38 @@ def initStatus(num_car):
     for i in range(num_car):
         tmp_dic = {}
         tmp_dic['name'] = taxi_name[i]
+        tmp_dic['crt_pos'] = i*12
         tmp_dic['trip'] = 0
         tmp_dic['dist'] = 0.0
         tmp_dic['income'] = 0
 
         rtn_status.append(tmp_dic)
 
+    rtn_status[0]['crt_pos'] = 12
+    rtn_status[1]['crt_pos'] = 0
+    rtn_status[2]['crt_pos'] = 24
+
     return rtn_status
 
-def updateStatus(c_status, fps, num_car):
+def updateStatus(c_status, tmp_list):
 
+    for i in range(len(c_status)):
 
-    return rtn_status
+        tmp_car_pos = c_status[i]['crt_pos']
+        ori_pos = tmp_car_pos
+        des_pos = tmp_list[ori_pos]
+        call_dist = int(np.abs(des_pos-ori_pos) / GRID_SIZE + np.abs(des_pos-ori_pos) % GRID_SIZE)
+
+        call_money = int(30+(call_dist-1)*14)
+
+        if des_pos > 0:
+            ## car-pos & call origin matched
+            c_status[i]['trip'] = c_status[i]['trip']+1
+            c_status[i]['dist'] = c_status[i]['dist'] + call_dist
+            c_status[i]['income'] = c_status[i]['income'] + call_money
+
+    return c_status
+
 
 def displayGrid(surf, grid = 5):
 #    surf.fill(WHITE)
@@ -202,9 +234,9 @@ def displayGrid(surf, grid = 5):
             displayText(surf, str(( i+1)+5*j ), rect_x+50, rect_y+50 )
 
 
-def displayText(surf, text, center_x, center_y):
+def displayText(surf, text, center_x, center_y, t_color=GREY):
     fontObj = pygame.font.Font('freesansbold.ttf', 40)
-    textSurfaceObj = fontObj.render(text, True, GREY, WHITE)
+    textSurfaceObj = fontObj.render(text, True, t_color, WHITE)
     textRectObj = textSurfaceObj.get_rect()
     textRectObj.center = (center_x, center_y)
     surf.blit(textSurfaceObj, textRectObj)
@@ -225,10 +257,11 @@ def displayCallRect(surf, tmp_list, grid=5):
         for j in range(grid):
             rect_x = start_x+i*(RECTSIZE-RECT_LINE_WIDTH/2)
             rect_y = start_y+j*(RECTSIZE-RECT_LINE_WIDTH/2)
-            if tmp_list[i+5*j] == 1:
+            ## If destination is defined
+            if tmp_list[i+5*j] > 0:
                 pygame.draw.rect(surf, RED, (rect_x , rect_y , RECTSIZE, RECTSIZE), RECT_LINE_WIDTH)
-
-
+                displayText(surf, str(tmp_list[i+5*j]), rect_x + 50, rect_y + 50, t_color=RED)
+                displayArrow(surf, rect_x + 28, rect_y + 50)
 
 
 def makeOriginProb(max_seq, RANDOM_SEED=42):
@@ -239,6 +272,11 @@ def makeOriginProb(max_seq, RANDOM_SEED=42):
         tmp_call = []
         for tmp_prob in origin_prob:
             tmp = np.random.binomial(1, tmp_prob, size=1)
+            ## if taxi is called
+            if tmp > 0 :
+                ## get destination from the prob
+                tmp = np.random.choice(np.arange(0, len(destin_prob)), p=destin_prob)
+
             tmp_call.append(tmp)
 
         rtn_array.append(tmp_call)
